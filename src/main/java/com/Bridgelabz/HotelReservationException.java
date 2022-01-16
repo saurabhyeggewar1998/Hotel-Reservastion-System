f58a62c7f27a33ea53215cbc6d419c4ec84df7c5
saurabhyeggewar1998 <yeggewar@gmail.com>
2022-01-16 07:20:00 +0530

    HotelReservationSystem Workshop3

---
+++ src/main/java/com/Bridgelabz/HotelReservationException.java
@@ -0,0 +1,16 @@
+package com.Bridgelabz;
+public class HotelReservationException extends RuntimeException{
+
+    enum ExceptionType{
+        ENTERED_NULL, ENTERED_EMPTY;
+    }
+
+    ExceptionType type;
+
+    public HotelReservationException(ExceptionType type, String message) {
+
+        super(message);
+        this.type = type;
+
+    }
+}
